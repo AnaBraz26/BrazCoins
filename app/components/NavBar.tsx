5ea305d3c5dafef5f277bfa853997125caaae6bf
Ana Caroline <212008482@aluno.unb.br>
2025-03-25 16:59:02 -0300

    Add:Tela inicial

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,15 +1,15 @@
 export default function NavBar() {
     return(
-        <div className="flex justify-between items-center h-20 bg-[#000000] px-14">
+        <div className="flex justify-between items-center h-20 bg-[#202216] px-14">
             <div className="flex items-center space-x-4">
                 <img className="w-45 cursor-pointer" src="BRAZ.png" alt="Brazcoins"/>
             </div>
 
         <div className="flex justify-between items-center">
-                <button className="flex m-5 items-center px-6 py-3 bg-yellow-500 text-white rounded-md"> Quem somos </button>
-                <button className="flex m-5 items-center px-6 py-3 bg-yellow-500 text-white rounded-md"> Serviços </button>
-                <button className="flex m-5 items-center px-6 py-3 bg-yellow-500 text-white rounded-md"> Produtos </button>
-                <button className="flex m-5 items-center px-6 py-3 bg-yellow-500 text-white rounded-md"> Contato </button>
+                <button className="flex m-5 items-center px-6 py-3 bg-[#C3AF20] border-white border-2 text-black font-bold rounded-md"> Quem somos </button>
+                <button className="flex m-5 items-center px-6 py-3 bg-[#C3AF20] border-white border-2 text-black font-bold rounded-md"> Serviços </button>
+                <button className="flex m-5 items-center px-6 py-3 bg-[#C3AF20] border-white border-2 text-black font-bold rounded-md"> Produtos </button>
+                <button className="flex m-5 items-center px-6 py-3 bg-[#C3AF20] border-white border-2 text-black font-bold rounded-md"> Contato </button>
             </div>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,46 @@ import NavBar from "./components/NavBar";
 
 export default function Home(){
   return(
+      
     <div>
         <NavBar></NavBar>
 
-        <div>
+        {/* chamada para entrar em Contatoe */}
+        <div className="flex justify-between items-center h-150 bg-[#C3AF20]">
         
         </div>
+
+        {/* sobre Brazcoins */}
+        <div className="flex justify-between items-center h-150 bg-[#FFFFFF]">
+
+        </div>
+
+        {/* serviços prestados */}
+        <div  className="flex justify-between items-center h-150 bg-[#C3AF20]">
+
+        </div>
+
+        {/* quem já comprou diz */}
+        <div  className="flex justify-between items-center h-150 bg-[#FFFFFF]">
+
+
+        </div>
+
+        <div className="border-b-3 border-black flex justify-center items-center h-60 bg-[#C3AF20]">
+          {/* sobre Brazcoins */}
+          <div className="flex justify-center px-50">
+            
+          </div>
+          {/* contato */}
+          <div className="flex justify-center px-60">
+             
+          </div>  
+          
+        </div>
+
+        <footer className="flex justify-center items-center h-10 bg-[#C3AF20]">
+          <h1 className="text-black font-bold items-center">© 2025 | Desenvolvido por Ana Caroline | Todos os direitos reservados | CNPJ: 00.000.000/0000-00 </h1>
+        </footer>
 
     </div>
   );
